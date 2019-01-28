94448f8bb64f972d9990c3dbfc921f80e1da2748
Petteri Peltokangas <petteri.peltokangas@edu.turkuamk.fi>
2019-01-28 15:20:35 +0200

    Add kunnat.js, clean up on leaflet.js

--- index/js/leaflet.js
+++ index/js/leaflet.js
@@ -1,20 +1,11 @@
 $(document).ready(function() {
-    let centerPoint = new L.LatLng(60.45, 22.26);
-    let mymap = new L.Map('mapid').setView(centerPoint, 13);
+    let centerPoint = new L.LatLng(60.45, 22.4);
+    let mymap = new L.Map('mapid').setView(centerPoint, 8);
     let osmOptions = {
         subdomains: ['a', 'b', 'c'],
 
     };
 
-    let marker = new L.Marker([60.46, 22.27]).addTo(mymap);
-    marker.bindPopup("Moro, oon popuppi").openPopup();
-
     let osm = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png ', osmOptions);
     osm.addTo(mymap);
-
-    function onMapClick(e) {
-        alert("You clicked the map at " + e.latLng);
-    }
-
-    mymap.on('click', onMapClick);
 });
