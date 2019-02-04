abefe65bf3fa347142cc994aa645ec457946fddb
Petteri Peltokangas <petteri.peltokangas@edu.turkuamk.fi>
2019-02-04 11:42:50 +0200

    -

--- index/js/leaflet.js
+++ index/js/leaflet.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
+    let bounds = kuntaParse("Aura");
+    let lBounds = latLngSwapper(bounds);
+
     let centerPoint = new L.LatLng(60.45, 22.4);
-    let mymap = new L.Map('mapid').setView(centerPoint, 8);
+    var mymap = new L.Map('mapid').setView(centerPoint, 8);
     let osmOptions = {
         subdomains: ['a', 'b', 'c'],
-
     };
-
     let osm = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png ', osmOptions);
     osm.addTo(mymap);
+
+    console.log(kuntaParse("Aura"));
+
+    let polygon = new L.Polygon(lBounds, {color: 'red'}).addTo(mymap);
+
+    testi("Turku");
 });
