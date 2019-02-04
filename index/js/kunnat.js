d230f9c24844055433fdfa0073c0fb95229bec01
Petteri Peltokangas <petteri.peltokangas@edu.turkuamk.fi>
2019-02-04 11:44:12 +0200

    Add geojson data to kuntaRajat.js, add parse and coordinate swapper functions to kunnat.js

--- index/js/kunnat.js
+++ index/js/kunnat.js
@@ -1,3 +1,25 @@
-var kuntienRajat = {
-    
-};
+function kuntaParse(kunta) {
+    for (let feature of kuntaInfo.features) {
+        if (feature.properties.name === kunta) {
+            return feature.geometry.coordinates[0];
+        }
+    }
+}
+
+function testi(a) {
+    for (let geo of kuntaInfo.features) {
+        if (geo.properties.name === a) {
+            console.log(geo.geometry);
+        }
+    }
+}
+
+function latLngSwapper(lonLat) {
+    let latLngs = [];
+    for (let i = 0; i < lonLat.length; i++) {
+        let alkio = new L.LatLng(lonLat[i][1], lonLat[i][0]);
+        latLngs.push(alkio);
+    }
+    return latLngs;
+}
+
